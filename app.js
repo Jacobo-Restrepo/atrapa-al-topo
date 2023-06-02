60ad1a4419b5612235aa16cd13b1e56be7c452a6
Jacobo-Restrepo <123768195+Jacobo-Restrepo@users.noreply.github.com>
2023-06-02 14:03:26 -0500

    5

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const cuadrado = document.querySelectorAll(".cuadrado");
+const cuadro = document.querySelectorAll(".cuadrado");
 const tiempofaltante = document.querySelector("#tiempo");
 let puntaje = document.getElementById("puntaje");
 
 let resultado = 0;
 let tiempoactual = tiempofaltante.textContent;
 
-function cuadradradoazar{
-  cuadrado.forEach(nombredeclase =>{
+function cuadrodoazar(){
+  cuadro.forEach((nombredeclase =>{
     nombredeclase.classList.remove("topo");
-  })
-  let posicionalazar = cuadrado[Math.floor(Math.random()*9)]
+  });
+  let posicionalazar = cuadro[Math.floor(Math.random()*9)]
   posicionalazar.classList.add("topo");
-  posiciontopo.posicionalazar.Id
+
+  posiciontopo=posicionalazar.id;
 }
-cuadrado.forEach(identificador=>{
+cuadro.forEach((identificador=>{
   identificador.addEventListener("click" ()=>{
     if (identificador.id===pociciontopo) {
-      resultado=resultado+1;
+      resultado= resultado + 1;
       puntaje.textContent=resultado;
       pociciontopo=null;
     }
-  })
-})
+  });
+});
 
 function movertopo (){
   tiempotopo =setInterval(cuadradradoazar,700);
