f408ae5c2fb9b86117ee567f6c5c2f6222f7d73e
Jacobo-Restrepo <123768195+Jacobo-Restrepo@users.noreply.github.com>
2023-06-05 12:44:21 -0500

    93544

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
-const cuadro = document.querySelectorAll(".cuadrado");
+const cuadrado = document.querySelectorAll(".cuadrado");
 const tiempofaltante = document.querySelector("#tiempo");
 let puntaje = document.getElementById("puntaje");
 
 let resultado = 0;
 let tiempoactual = tiempofaltante.textContent;
 
-function cuadrodoazar(){
-  cuadro.forEach((nombredeclase =>{
+function cuadradradoazar() {
+  cuadrado.forEach((nombredeclase) => {
     nombredeclase.classList.remove("topo");
   });
-  let posicionalazar = cuadro[Math.floor(Math.random()*9)]
+  let posicionalazar = cuadrado[Math.floor(Math.random() * 9)];
   posicionalazar.classList.add("topo");
-
-  posiciontopo=posicionalazar.id;
+  
+  posiciontopo = posicionalazar.id;
 }
-cuadro.forEach((identificador=>{
-  identificador.addEventListener("click" ()=>{
-    if (identificador.id===pociciontopo) {
-      resultado= resultado + 1;
-      puntaje.textContent=resultado;
-      pociciontopo=null;
+
+cuadrado.forEach((identificador) => {
+  identificador.addEventListener("click", () => {
+    if (identificador.id === pociciontopo) {
+      resultado = resultado + 1;
+      puntaje.textContent = resultado;
+      pociciontopo = null;
     }
   });
 });
 
-function movertopo (){
-  tiempotopo =setInterval(cuadradradoazar,700);
+function movertopo() {
+  tiempotopo = setInterval(cuadradradoazar, 700);
 }
 movertopo();
 
-function cuentaregresiva(){
-  tiempoactual--;
-  tiempofaltante.textContent=tiempoactual;
-  if(tiempoactual===0);{
-    clearInterval(idtiempo);
-    clearInterval(timepotopo);
-    alert ("Se acabó el tiempo, tu puntaje fue:" + resultado + "topos atrapados");
-  }
-
-}
+function cuentaregresiva() 
